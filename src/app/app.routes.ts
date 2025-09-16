22b7446c04d2a7dcb9fcf1d1fdd2c7416c48ab87
Deniz Tufan ARSLAN <dtufan.arslan@stm.com.tr>
2025-09-16 10:31:12 +0200

    add routing + 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { About } from './routes/about/about';
+import { Error } from './routes/error/error';
+import { Index } from './routes/index';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component: Index,
+        path: ''
+    },
+    {
+        component: About,
+        path: 'about'
+    },
+    {
+        component: Error,
+        path: '**'
+    }
+];
