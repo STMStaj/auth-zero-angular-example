1a2ab53ed889b6b96044bb9bbdf2bee5726c0e70
Furkan CANPINAR <furkan.canpinar@stm.com.tr>
2025-09-23 12:00:15 +0300

    Added Css to fetch page + added the prime ng table

--- src/app/routes/fetch/fetch.ts
+++ src/app/routes/fetch/fetch.ts
@@ -1,43 +1,45 @@
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe, CurrencyPipe } from '@angular/common';
 import { Component, OnInit, signal, WritableSignal } from '@angular/core';
-import { Observer } from 'rxjs';
+import { take } from 'rxjs';
 import { FetchService } from '../../services/fetch.service';
 import { AuthService, User } from '@auth0/auth0-angular';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { TableModule } from 'primeng/table';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './fetch.html',
-    styleUrl: './fetch.css',
-    imports: [JsonPipe, AsyncPipe, ProgressSpinnerModule]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './fetch.html',
+  styleUrls: ['./fetch.css'],
+  imports: [
+    JsonPipe,
+    AsyncPipe,
+    ProgressSpinnerModule,
+    TableModule,
+    CurrencyPipe
+  ],
 })
 export class Fetch implements OnInit {
-    private callback: Observer<Object> = {
-        error: (err) => {
-            console.error('Error fetching data:', err);
-        },
-        next: (value) => {
-            this.data.set(value);
-        },
-        complete: () => {
-        },
-    }
-    data: WritableSignal<any> = signal({});
-    Object = Object;
+  data: WritableSignal<any[]> = signal([]);
 
-    constructor(public auth: AuthService, private service: FetchService) { }
+  constructor(public auth: AuthService, private service: FetchService) {}
 
-    ngOnInit() {
-        this.auth.user$.subscribe(u => {
-                if (u) {
-                    this.fetchData(u);
-                }
-            }
-        );
-    }
+  ngOnInit() {
+    this.auth.user$.pipe(take(1)).subscribe((u) => {
+      if (u) {
+        this.fetchData(u);
+      }
+    });
+  }
 
-    fetchData(user: User) {
-        console.log("Request by " + user.email);
-        this.service.getData().subscribe(this.callback);
-    }
+  fetchData(user: User) {
+    console.log('Request by ' + user.email);
+    this.service.getData().subscribe({
+      next: (value) => {
+        // Ensure it's an array
+        this.data.set(Array.isArray(value) ? value : value.products);
+      },
+      error: (err) => console.error('Error fetching data:', err),
+    });
+  }
 }
